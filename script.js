833f3d91ed6fa4e350c4607eaf7ad679adde6c51
Jake <taves.hemn201@gmail.com>
2023-05-12 19:51:28 +0300

    good commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Initialize ScrollReveal
+ScrollReveal({
+    distance: '20px',
+    duration: 800,
+    easing: 'ease-in-out',
+    mobile: false
+  });
+  
+  // Configure the elements to animate
+  ScrollReveal().reveal('.reveal', {
+    origin: 'left',
+    interval: 200
+  });
+  
